c25c3e6c30c85c412fd1cc34d74f4a2e57d22859
Arvin Agas <arvagas@gmail.com>
2019-09-16 16:38:24 -0400

    added logout endpoint

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -28,4 +28,13 @@ router.post('/login', (req, res) => {
     .catch(err => res.status(500).json({ message: 'error logging in' }))
 })
 
+router.get('/logout', (req, res) => {
+  if (req.session.user) {
+    req.session.destroy(err => {
+      if (err) res.json({ message: 'error logging out' })
+      else res.status(200).json({ message: 'log out successful' })
+    })
+  } else res.json({ message: 'not logged in' })
+})
+
 module.exports = router
