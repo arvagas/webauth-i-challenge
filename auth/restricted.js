512c51890c531cdfdb1a7054382304e3d9c2bb9e
Arvin Agas <arvagas@gmail.com>
2019-09-16 14:21:21 -0400

    fixed restricted middleware

--- auth/restricted.js
+++ auth/restricted.js
@@ -9,8 +9,8 @@ module.exports = function restricted(req, res, next) {
     Users.findBy({ user })
       .first()
       .then(usr => {
-        console.log(usr)
-        next()
+        if (usr && bcrypt.compareSync(password, usr.password)) next()
+        else res.status(401).json({ message: 'You shall not pass!' })
       })
       .catch(err => res.status(500).json({ message: 'error validating credentials' }))
   } else res.status(404).json({ message: 'please provide valid credentials' })
