4a7a3fee98d5d21949403d679085ec7fd974ae30
Arvin Agas <arvagas@gmail.com>
2019-09-17 11:56:23 -0400

    set up empty reducer

---
+++ client/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+  users: [],
+  error: '',
+  isLoggedIn: false,
+  isLoading: false,
+}
+
+export const reducer = (state = initialState, action) => {
+  switch(action) {
+    default :
+      return state
+  }
+}
