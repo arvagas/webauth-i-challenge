671fa4a17fd5456e14eb35272752ee8cb5c1e0ad
Arvin Agas <arvagas@gmail.com>
2019-09-18 12:15:45 -0400

    added navbar to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import NavBar from './components/NavBar'
 import AppRouter from './components/router/AppRouter'
 
 import './App.css'
@@ -7,6 +8,8 @@ import './App.css'
 function App() {
   return (
     <div>
+      <NavBar />
+
       <AppRouter />
     </div>
   )
